813c49a3d0035bda0cc04dc12836508359efac53
pavlonk.04 <pavlonk.04@spst.eu>
2022-09-05 09:14:32 +0200

    p01

--- p01/Form1.cs
+++ p01/Form1.cs
@@ -16,5 +16,43 @@ namespace p01
         {
             InitializeComponent();
         }
+
+        List<int> list = new List<int>();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n;
+            if (textBoxN.Text != string.Empty)
+            {
+                n = Convert.ToInt32(textBoxN.Text);
+                Random rnd = new Random();
+                for (int i = 0; i < n; i++)
+                {
+                    list.Add(rnd.Next(1, 101));
+                    listBox1.Items.Add(list[i]);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            list.Sort();
+            string full = list[0].ToString();
+            for (int i = 1; i < list.Count; i++)
+            {
+                full += ", " + list[i].ToString();
+            }
+            MessageBox.Show(full);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            list.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
